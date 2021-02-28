147e8c7d622bc90532df70d06a3085d5b0849225
Jerome Simmons <jeromesimmons@Jeromes-MacBook-Pro.local>
2021-02-28 14:38:29 -0800

    adding updated file structure.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -11,7 +11,15 @@ L.circleMarker([34.0522, -118.2437], {
     fillOpacity: 0.75 
  }).addTo(map);
 
-// We create the tile layer that will be the background of our map.
+// adds a circle on the map for LA, California that has a radius of 300M.
+//  L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: "yellow",
+//     fillOpacity: 0.75 
+//  }).addTo(map);
+
+// We create the tile layer that will be the background of our map. id 'Mapbox/streets-v11' would create a 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
